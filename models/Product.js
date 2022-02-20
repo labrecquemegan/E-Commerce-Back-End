f832d510edf9a60d78a2c059b457dfdbd739f40e
ML <labrecquemegan@hotmail.com>
2022-02-20 12:25:01 -0500

    started adding in models

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
   },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+
+  },
+  stock: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isInteger: true,
+    }
+  },
+  category_id: {
+type: DataTypes.INTEGER,
+references: {
+  model: 'Category',
+  key: 'id',
+  unique: false
+}
+  }
+},
   {
     sequelize,
     timestamps: false,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsto(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+})
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+
+})
 
 module.exports = {
   Product,
