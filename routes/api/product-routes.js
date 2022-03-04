427042ce93f4153fb9e3c74535a897f007089ff9
ML <labrecquemegan@hotmail.com>
2022-03-04 11:34:02 -0500

    took away await functions, still can't seed data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
+    const productData = Product.findAll({
       include: [{Model: Category}, {model: Tag}]
     });
     res.status(200).json(productData);
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
     try {
-      const productData = await Product.findByPk(req.params.id, {
+      const productData = Product.findByPk(req.params.id, {
         include: [{ model: Category}, {model: Tag}]
       });
 
@@ -106,7 +106,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
     try {
-      let deletedProduct = await Product.destroy({
+      let deletedProduct = Product.destroy({
         where: {
           id: req.params.id
         }
